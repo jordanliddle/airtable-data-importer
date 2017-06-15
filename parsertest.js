01363ee4de2b03b3aa7ed1011f236242379ab667
Jordan Liddle <jordan.liddle@shopify.com>
2017-06-15 18:01:16 -0400

    sets to stable state

--- parsertest.js
+++ parsertest.js
@@ -6,7 +6,9 @@ var client = redis.createClient();
 const pry = require('pryjs')
 var Airtable = require('airtable');
 var base = new Airtable({apiKey: 'keyK7hube384uNDmc'}).base('appNwUk0MQdtk41vZ');
-
+var bluebird = require("bluebird");
+bluebird.promisifyAll(redis.RedisClient.prototype);
+bluebird.promisifyAll(redis.Multi.prototype);
 client.on('connect', function() {
     console.log('connected');
 });
@@ -37,8 +39,8 @@ base('DATA').select({
 let csvstream = csv.fromPath("sandbox.csv", { headers: true })
     .on("data", function (row) {
         csvstream.pause();
-        client.get(row['shopify partner id'], function(err, reply) {
-          console.log(reply);
+        client.getAsync(row['Shopify Partner Id']).then(function(res) {
+          console.log(res); // => 'bar'
         });
         csvstream.resume();
     })
@@ -49,3 +51,29 @@ let csvstream = csv.fromPath("sandbox.csv", { headers: true })
         console.log(error)
     });
 
+
+// base('DATA').replace(res, {
+//   "Shopify Partner Id": row["Shopify Partner Id"],
+//   "Partner Company Name": [
+//     row["Partner Company Name"]
+//   ],
+//   "partner url": row["partner url"],
+//   "Partner Is Current Shopify Expert": row["Partner Is Current Shopify Expert"],
+//   "Partner Email": row["Partner Email"],
+//   "Partner Contact First Name": row["Partner Contact First Name"],
+//   "Partner Contact Last Name": "Kogan",
+//   "Current Partner Manager": "Lauren Helstab",
+//   "Partner Country": "US",
+//   "Partner City": "Philadelphia",
+//   "Partner Province": "PA",
+//   "Partner Created At (Est)": "13-09-01 10:22",
+//   "number of development shops created": "19",
+//   "Number Of New Leads": "17",
+//   "net customers change": "8",
+//   "Net Mrr Change": "529",
+//   "Shopify Partner Id 2": "140467",
+//   "Shopify Partner Id 3": "140467"
+// }, function(err, record) {
+//     if (err) { console.error(err); return; }
+//     console.log(record.get('Shopify Partner Id'));
+// });
