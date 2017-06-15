6bfbab016717e9f3912ebb46b88ba02306c7a772
Jordan Liddle <jordan.liddle@shopify.com>
2017-06-15 16:52:02 -0400

    adds redis

--- parsertest.js
+++ parsertest.js
@@ -1,29 +1,27 @@
 
 var fs = require('fs');
 var csv = require("fast-csv");
-const low = require('lowdb');
-const db = low('db.json');
+var redis = require('redis');
+var client = redis.createClient();
 const pry = require('pryjs')
 var Airtable = require('airtable');
 var base = new Airtable({apiKey: 'keyK7hube384uNDmc'}).base('appNwUk0MQdtk41vZ');
 
-// Set some defaults if your JSON file is empty
-db.defaults({ alldata: [] })
-  .write()
+client.on('connect', function() {
+    console.log('connected');
+});
 
 
 base('DATA').select({
       // Selecting the first 3 records in Grid view:
-      maxRecords: 100,
+      maxRecords: 10,
       view: "Grid view"
   }).eachPage(function page(records, fetchNextPage) {
       // This function (`page`) will get called for each page of records.
 
       records.forEach(function(record) {
-        db.get('alldata')
-          .push({
-            primaryId: record.id, partnerId: record.fields["Shopify Partner Id"]})
-          .write()
+        console.log('Retrieved', record.get('Shopify Partner Id'));
+        client.set(record.fields["Shopify Partner Id"], record.id);
       });
 
       // To fetch the next page of records, call `fetchNextPage`.
@@ -36,13 +34,18 @@ base('DATA').select({
   });
 
 
-var stream = fs.createReadStream("sandbox.csv");
-var csvStream = csv()
-    .on("data", function(data){
-         console.log(data);
+let csvstream = csv.fromPath("sandbox.csv", { headers: true })
+    .on("data", function (row) {
+        csvstream.pause();
+        client.get(row['shopify partner id'], function(err, reply) {
+          console.log(reply);
+        });
+        csvstream.resume();
     })
-    .on("end", function(){
-         console.log("done");
+    .on("end", function () {
+        console.log("We are done!")
+    })
+    .on("error", function (error) {
+        console.log(error)
     });
-stream.pipe(csvStream);
 
