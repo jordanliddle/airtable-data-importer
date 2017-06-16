b770498e36b9b0f5b72ac786af1ca5772905fcef
Jordan Liddle <jordan.liddle@shopify.com>
2017-06-15 20:23:59 -0400

    adds promises

--- parsertest.js
+++ parsertest.js
@@ -13,43 +13,50 @@ client.on('connect', function() {
     console.log('connected');
 });
 
+function pullRecords() {
+  return new Promise(function (resolve, reject) {
+    base('DATA').select({
+          // Selecting the first 3 records in Grid view:
+          maxRecords: 10,
+          view: "Grid view"
+      }).eachPage(function page(records, fetchNextPage) {
+          // This function (`page`) will get called for each page of records.
 
-base('DATA').select({
-      // Selecting the first 3 records in Grid view:
-      maxRecords: 10,
-      view: "Grid view"
-  }).eachPage(function page(records, fetchNextPage) {
-      // This function (`page`) will get called for each page of records.
+          records.forEach(function(record) {
+            console.log('Retrieved', record.get('Shopify Partner Id'));
+            client.set(record.fields["Shopify Partner Id"], record.id);
+          });
 
-      records.forEach(function(record) {
-        console.log('Retrieved', record.get('Shopify Partner Id'));
-        client.set(record.fields["Shopify Partner Id"], record.id);
+          // To fetch the next page of records, call `fetchNextPage`.
+          // If there are more records, `page` will get called again.
+          // If there are no more records, `done` will get called.
+          fetchNextPage();
+
+      }, function done(err) {
+          resolve(updateRecords())
+          if (err) { console.error(err); return; }
       });
+  })
+}
 
-      // To fetch the next page of records, call `fetchNextPage`.
-      // If there are more records, `page` will get called again.
-      // If there are no more records, `done` will get called.
-      fetchNextPage();
+function updateRecords() {
+  let csvstream = csv.fromPath("sandbox.csv", { headers: true })
+      .on("data", function (row) {
+          csvstream.pause();
+          client.getAsync(row['Shopify Partner Id']).then(function(res) {
+            console.log(res); // => 'bar'
+          });
+          csvstream.resume();
+      })
+      .on("end", function () {
+          console.log("We are done!")
+      })
+      .on("error", function (error) {
+          console.log(error)
+      });
+}
 
-  }, function done(err) {
-      if (err) { console.error(err); return; }
-  });
-
-
-let csvstream = csv.fromPath("sandbox.csv", { headers: true })
-    .on("data", function (row) {
-        csvstream.pause();
-        client.getAsync(row['Shopify Partner Id']).then(function(res) {
-          console.log(res); // => 'bar'
-        });
-        csvstream.resume();
-    })
-    .on("end", function () {
-        console.log("We are done!")
-    })
-    .on("error", function (error) {
-        console.log(error)
-    });
+pullRecords();
 
 
 // base('DATA').replace(res, {
@@ -61,18 +68,18 @@ let csvstream = csv.fromPath("sandbox.csv", { headers: true })
 //   "Partner Is Current Shopify Expert": row["Partner Is Current Shopify Expert"],
 //   "Partner Email": row["Partner Email"],
 //   "Partner Contact First Name": row["Partner Contact First Name"],
-//   "Partner Contact Last Name": "Kogan",
-//   "Current Partner Manager": "Lauren Helstab",
-//   "Partner Country": "US",
-//   "Partner City": "Philadelphia",
-//   "Partner Province": "PA",
-//   "Partner Created At (Est)": "13-09-01 10:22",
-//   "number of development shops created": "19",
-//   "Number Of New Leads": "17",
-//   "net customers change": "8",
-//   "Net Mrr Change": "529",
-//   "Shopify Partner Id 2": "140467",
-//   "Shopify Partner Id 3": "140467"
+//   "Partner Contact Last Name": row["Partner Contact Last Name"],
+//   "Current Partner Manager": row["Current Partner Manager"],
+//   "Partner Country": row["Partner Country"],
+//   "Partner City": row["Partner City"],
+//   "Partner Province": row["Partner Province"],
+//   "Partner Created At (Est)": row["Partner Created At (Est)"],
+//   "number of development shops created": row["number of development shops created"],
+//   "Number Of New Leads": row["Number Of New Leads"],
+//   "net customers change": row["net customers change"],
+//   "Net Mrr Change": row["Net Mrr Change"],
+//   "Shopify Partner Id 2": row["Shopify Partner Id 2"],
+//   "Shopify Partner Id 3": row["Shopify Partner Id 3"]
 // }, function(err, record) {
 //     if (err) { console.error(err); return; }
 //     console.log(record.get('Shopify Partner Id'));
