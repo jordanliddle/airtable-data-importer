f302b097404e2804b9a1337991d35f75db5b9e88
Jordan Liddle <jordan.liddle@shopify.com>
2017-12-13 10:00:17 -0500

    updates data points

--- app.js
+++ app.js
@@ -11,6 +11,7 @@ const API_KEY = process.env.API_KEY,
 const Airtable = require('airtable');
 let base = new Airtable({apiKey: API_KEY}).base('appxGOGhkQ6ueTKjI');
 
+
 const bluebird = require("bluebird");
 bluebird.promisifyAll(redis.RedisClient.prototype);
 
@@ -21,17 +22,16 @@ client.on('connect', function() {
 });
 
 function pullAllRecords() {
-    base(BASE_NAME).select({
+    base('Imported table').select({
           // ensure the view is correct
-           maxRecords: 50,
+           maxRecords: 10,
           view: "Grid view"
       }).eachPage(function page(records, fetchNextPage) {
           records.forEach(function(record) {
             // log each requested record
             console.log('Retrieved', record.get('app id'));
             // save the record to Redis
-            client.set(record.fields["app id"], record.id);
-            // client.set(record.fields["re"], record.app_relationship)
+            client.hmset(record.fields["app id"], { record_id: record.id, app_id: record.fields["app name (app id) no commas"][0] });
           });
           fetchNextPage();
       }, function done(err) {
@@ -73,14 +73,26 @@ function delay(delay) {
 }
 
 function performUpdate(data) {
+  // getID().then(function(res) {
+  // })
 
   // get record primary key in Redis
-  return client.getAsync(data['app id'])
+  return client.hmgetAsync(data['app id'], "record_id", "app_id")
+  // .then(function(res) {
+  //   return client.getAsync(data['app id'])
+  // }).then(function(res) {
+  //   return client.getAsync(res)
+  // })
   .then(function(res) {
+    // const x = bluebird.promisify(client.get(data['app id']))
+    // const application_id = bluebird.promisify(client.get(x))
+    let application_id = res[1].split();
+
+    console.log(application_id)
+    console.log(res[0])
     let record = {
-      "app name":   data["app name"]
-      "app name (app id) no commas"                                                 data["app name"],
-      "handle":                                             data["handle"],
+      "app id":                                                data["app_id"],
+      "app name (app id) no commas":                                               application_id,
       "app_store_slug":                                              data["app_store_slug"],
       "recommendable":                                              data["recommendable"],
       "active installs":                        data["active installs"],
@@ -89,21 +101,31 @@ function performUpdate(data) {
       "average_rating":                                             data["average_rating"],
       "number_of_reviews_in_average":                                         data["number_of_reviews_in_average"],
       "first published":                                       data["first published"],
+      "first submitted":                                       data["first submitted"],
+      "last submitted":                                       data["last submitted"],
       "country":                      data["country"],
       "continent":                                      data["continent"],
       "self reported pricing":                                           data["self reported pricing"],
       "app category":                                    data["app category"]
     }
 
-    if (res == null) {
+    if (res[0] == null) {
       const fn = bluebird.promisify(base('Imported table').create);
       return fn(record);
     } else {
       const fn = bluebird.promisify(base('Imported table').update);
-      return fn(res, record);
+      return fn(res[0], record);
     }
   }).catch(function(e) {
     console.log(e)
+  })
+}
+
+function getID() {
+  client.getAsync(data['app id']).then(function(res){
+    client.getAsync(res).then(function(res) {
+       return res
+    })
   })
 }
 
